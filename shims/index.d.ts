bb3b1ee474a15cec1a817f4ece6ae77be416c3ea
Julius <juliusbartolomejr@gmail.com>
2024-06-13 22:02:49 +0800

    CHORE: Initial appointment page

--- shims/index.d.ts
+++ shims/index.d.ts
@@ -1,2 +1,7 @@
-export type TSuffix = 'II' | 'III' | 'IV' | 'Jr.' | 'Sr.'
-export type TGender = 'female' | 'male'
+/* eslint-disable */
+export type TSuffix = 'Jr.' | 'Sr.' | 'II' | 'III' | 'IV'
+export type TGender = 'male' | 'female'
+export type TBrandColor =  'neutral' | 'primary' | 'secondary' | 'accent' | 'ghost' | 'link'
+export type TStateColor =  'info' | 'success' | 'warning' | 'error'
+export type TSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl'
+export type TSolarIconVariant = 'bold' | 'bold-duotone' | 'broken' | 'line-duotone' | 'linear' | 'outline'
